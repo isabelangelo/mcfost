9867bd368a9662ce515e386dffd6c12ef98e14e4
isabelangelo <isabelangelo@berkeley.edu>
2019-03-11 15:14:53 -0700

    added 1d probability plot function

--- grid_statistics.py
+++ grid_statistics.py
@@ -3,13 +3,41 @@ from model_grid import grid_parameters
 
 d = fits.open('binary_array.fits')[0].data
 
-def plot_parameter_probabilities(paramstrx,paramstry):
+def plotP_1D(paramstr):
+    """
+    plot histogram for parameter showing probability count
+    """
+    # update dictionary to inclue inclinations
+    param_dict = model_grid.grid_parameters.copy()
+    param_dict['inc']=[str(i)[:2] for i in list(Model(0).inclinations)]
+    
+    # get list of parameter values
+    param_list = param_dict[paramstr]
+    
+    # collapse axes
+    keys = list(param_dict.keys())
+    axes = [7,6,5,4,3,2,1,0]
+    axes.remove(keys.index(paramstr))
+    values = np.sum(d,axis=tuple(axes)) # do we want to normalize?
+    
+    ## spacing based on distance
+    #plt.step(param_list,values,where='mid')
+    # forced even spacing
+    a = np.arange(0,len(param_list))
+    plt.step(a,values,where='mid',color='darkslateblue')
+    plt.xticks(a,param_list)
+    
+    plt.xlabel(paramstr);plt.ylabel('$N_{edge-on}$')
+    plt.show()
+    
+    
+def plotP_2D(paramstrx,paramstry):
     """
     plot correlation image for 2 input parameters
     """
     # update dictionary to inclue inclinations
     param_dict = model_grid.grid_parameters.copy()
-    param_dict['inc']=[str(i)[:2] for i in list(Model(0).inclinations)] # change labels
+    param_dict['inc']=[str(i)[:2] for i in list(Model(0).inclinations)]
 
     # get list of parameter values
     param_listx = param_dict[paramstrx]
@@ -45,6 +73,3 @@ def plot_parameter_probabilities(paramstrx,paramstry):
     #cax = plt.axes([0.85, 0.1, 0.075, 0.8])
     #plt.colorbar(cax=cax)
     
-    
-#TO DO: make this so you can correlate any input two?
-#maybe the default can be inclination?
