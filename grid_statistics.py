53c23d2c4e012dfbff858c94adc58176b74c35b3
isabelangelo <isabelangelo@berkeley.edu>
2019-03-11 12:41:19 -0700

    handling of 2 input parameters for correlation plot

--- grid_statistics.py
+++ grid_statistics.py
@@ -2,33 +2,45 @@ from models import *
 from model_grid import grid_parameters
 
 d = fits.open('binary_array.fits')[0].data
-n_models = d.size/15.
 
-def plot_parameter_probabilities(paramstr, cmap='inferno'):
+def plot_parameter_probabilities(paramstrx,paramstry):
+    """
+    plot correlation image for 2 input parameters
+    """
+    # update dictionary to inclue inclinations
+    param_dict = model_grid.grid_parameters.copy()
+    param_dict['inc']=[str(i)[:2] for i in list(Model(0).inclinations)] # change labels
 
     # get list of parameter values
-    param_list = model_grid.grid_parameters[paramstr]
+    param_listx = param_dict[paramstrx]
+    param_listy = param_dict[paramstry]
     
     # generate axes to collapse
-    keys = list(grid_parameters.keys()) # you can import this instead
-    axes = [6,5,4,3,2,1,0]
-    axes.remove(keys.index(paramstr))
-
+    keys = list(param_dict.keys())
+    axes = [7,6,5,4,3,2,1,0]
+    axes.remove(keys.index(paramstrx))
+    axes.remove(keys.index(paramstry))
+    
     # collapse into 2D array and normalize
     values = np.sum(d,axis=tuple(axes))
-    max = n_models/len(param_list)
+    max = d.size/(len(param_listx)*len(param_listy))
     arr = values/max
-
-    # determine tick labels
-    inc = [str(i)[:2] for i in Model(0).inclinations]
-    dm = [str(d) for d in param_list]
     
-    plt.imshow(arr, vmin=0, vmax=1, cmap=cmap, aspect='auto')
-    plt.xticks(np.arange(0,15,1),inc);plt.yticks(np.arange(0,len(param_list)),dm)
-    plt.ylabel(paramstr);plt.xlabel('inclination')
-    plt.title(paramstr+' edge-on probabilities')
+    # determine tick labels
+    labelsx = [str(i) for i in param_listx]
+    labelsy = [str(i) for i in param_listy]
+    
+    # flip array so that labels will match values
+    if keys.index(paramstrx)<keys.index(paramstry):
+        arr = arr.T
+ 
+    plt.xticks(np.arange(0,len(param_listx)),labelsx)
+    plt.yticks(np.arange(0,len(param_listy)),labelsy)
+    plt.xlabel(paramstrx);plt.ylabel(paramstry)
+    
+    plt.imshow(arr, vmin=0, vmax=1, aspect='auto')
     plt.colorbar();plt.show()
-
+    
     # shared colorbar for subplots
     #cax = plt.axes([0.85, 0.1, 0.075, 0.8])
     #plt.colorbar(cax=cax)
