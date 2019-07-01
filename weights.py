5977b992537f9df82376905946457e8aacca354a
isabelangelo <isabelangelo@berkeley.edu>
2019-07-01 17:51:40 -0400

    Added weights.py

---
+++ weights.py
@@ -0,0 +1,4 @@
+
+# dust mass weights (these are very similar)
+mass_weights_lupus = [0.42140561, 0.3071352 , 0.1928648 , 0.07859439, 0.        ]
+mass_weights_taurus = [0.43447259, 0.31149086, 0.18850914, 0.06552741, 0.        ]
